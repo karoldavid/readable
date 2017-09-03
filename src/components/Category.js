7d177754f23b8729457d0f7819861095a4e36133
karoldavid <k.zysk@zoho.com>
2017-09-03 17:52:39 +0200

    feat: add back to main page navigation link to category page

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Category extends Component {
 	render() {
 		const category = this.props.match.params.catID
 		return(
-			<div>Category: {category}</div>
+			<div>
+				<div>Category: {category}</div>
+				<Link to="/">Back to Main</Link>
+			</div>
 		)
 	}
 }
