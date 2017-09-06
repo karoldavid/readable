ad7dfd562aeec12a7fdbdfe5af715b6457e412a5
karoldavid <k.zysk@zoho.com>
2017-09-06 13:07:40 +0200

    feat: add title input field to create nre post form

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,11 +1,32 @@
 import React, {Component } from 'react'
+import { Field, reduxForm, initialize } from 'redux-form'
+import { connect } from 'react-redux'
 
 class NewPost extends Component {
+
+
+
 	render() {
 		return(
-			<div>New Post!</div>
+			<div>
+				<form>
+					<Field name="title" type="text" component={renderField} label="title"/>
+				</form>
+			</div>
 		)
 	}
 }
 
-export default NewPost
+const form = reduxForm({
+	form: NewPost
+})
+
+const renderField = field => (
+    <div>
+      <label>{field.input.label}</label>
+      <input {...field.input}/>
+      {field.touched && field.error && <div className="error">{field.error}</div>}
+    </div>
+);
+
+export default connect(null, null)(form(NewPost));
