576758ea0503269fd1993773e69264df43d3ec89
karoldavid <k.zysk@zoho.com>
2017-09-08 12:06:29 +0200

    feat: redirect to post view when cancel button is hit

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 class EditPost extends Component {
+
 	render() {
+
+		const { post } = this.props
+
 		return(
 			<div>Edit Post!
 				<button type="submit" className="btn waves-effect waves-light">Save Post</button>
-  				<Link to="/"><button className="btn waves-effect waves-light">Cancel</button></Link>
+  				<Link to={{ pathname: `/posts/${post.id}`}}><button className="btn waves-effect waves-light">Cancel</button></Link>
   			</div>
 		)
 	}
 }
 
-function dispatchStateToProps( { post }) {
+function mapStateToProps( { post }) {
 	return { post }
 }
 
-export default EditPost
+export default connect(mapStateToProps, null)(EditPost) 
