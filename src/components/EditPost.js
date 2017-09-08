e09a1b7281b7bdc77da30fceafa92ad6d99dc2fc
karoldavid <k.zysk@zoho.com>
2017-09-08 11:48:49 +0200

    feat: add save and cancel button

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class EditPost extends Component {
 	render() {
 		return(
-			<div>Edit Post!</div>
-
+			<div>Edit Post!
+				<button type="submit" className="btn waves-effect waves-light">Save Post</button>
+  				<Link to="/"><button className="btn waves-effect waves-light">Cancel</button></Link>
+  			</div>
 		)
 	}
 }
