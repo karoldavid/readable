3194f0ff87cace8b2c093ed6ba623e82a4397dc7
karoldavid <k.zysk@zoho.com>
2017-09-08 12:27:28 +0200

    feat: edit form shows currently selected post data

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,17 +1,77 @@
 import React, { Component } from 'react'
+import { Field, reduxForm, initialize } from 'redux-form'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 class EditPost extends Component {
 
+	componentDidMount() {
+  		this.handleInitialize();
+	}
+
+	handleInitialize() {
+	 	const postData = {
+    		"title": this.props.post.title,
+    		"category": this.props.post.category,
+    		"body": this.props.post.body,
+    		"author": this.props.post.author,
+  		}
+
+  		this.props.initialize(postData)
+	}
+
+	renderField(field) {
+		return (
+	    	<div>
+	      		<label>{field.input.label}</label>
+	      		<input {...field.input}/>
+	      		{field.touched && field.error && <div className="error">{field.error}</div>}
+	    	</div>
+	    )
+	}
+
+	handleFormSubmit(params) {
+		console.log(params)
+	}
+
 	render() {
 
-		const { post } = this.props
+		const { post, handleSubmit } = this.props
 
 		return(
-			<div>Edit Post!
-				<button type="submit" className="btn waves-effect waves-light">Save Post</button>
-  				<Link to={{ pathname: `/posts/${post.id}`}}><button className="btn waves-effect waves-light">Cancel</button></Link>
+			<div>
+				<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+					<Field
+						name="title"
+						type="text"
+						label="Post Title"
+						className="input-field"
+						component={this.renderField}
+					/>
+					<Field
+						name="category"
+						type="text"
+						label="Post Category"
+						className="input-field"
+						component={this.renderField}
+					/>
+					<Field
+						name="body"
+						type="text"
+						label="Post Content"
+						className="input-field"
+						component={this.renderField}
+					/>
+					<Field
+						name="author"
+						type="text"
+						label="Author Name"
+						className="input-field"
+						component={this.renderField}
+					/>
+					<button type="submit" className="btn waves-effect waves-light">Save Post</button>
+  					<Link to={{ pathname: `/posts/${post.id}`}}><button className="btn waves-effect waves-light">Cancel</button></Link>
+				</form>
   			</div>
 		)
 	}
@@ -21,4 +81,8 @@ function mapStateToProps( { post }) {
 	return { post }
 }
 
-export default connect(mapStateToProps, null)(EditPost) 
+export default reduxForm({
+	form: 'EditPostForm'
+})(
+	connect(mapStateToProps, null)(EditPost)
+)
