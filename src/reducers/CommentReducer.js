0e8cd38239acb2ead79cbea31a818bc08dfea5f9
karoldavid <k.zysk@zoho.com>
2017-09-14 12:29:34 +0200

    refactor: move post and comment  reducer to own files

--- src/reducers/CommentReducer.js
+++ src/reducers/CommentReducer.js
@@ -1,5 +1,41 @@
-const initialCommentsState = {
+import {
+	ADD_COMMENT,
+	GET_COMMENTS,
+	DELETE_COMMENT,
+	SAVE_MOD_COMMENT,
+	CHANGE_COMMENT_VOTESCORE,
+	SORT_BY_DIRECTION,
+	SORT_BY_CATEGORY
+} from '../actions'
+
+import _ from 'lodash'
+
+const initialComments = {
   comments: [],
   commentsOrderBy: 'timestamp',
   commentsOrderDirection: 'desc',
+}
+
+export function comments(state = initialComments, action) {
+	const comments = action.comments ? action.comments : state.comments
+	const category = 'timestap'
+	const direction = 'desc'
+
+	switch (action.type) {
+		case ADD_COMMENT:
+			return state.concat(action.comment)
+		case GET_COMMENTS:
+			const comments = action.comments
+			return { ...state, comments: _.orderBy(comments, [category],[direction]) }
+		case DELETE_COMMENT:
+			const url = action.payload.url
+		    const id = url.substr(url.indexOf("comments/") + ("comments/".length))
+			return state.filter((comment) => id !== comment.id)
+		case SAVE_MOD_COMMENT:
+			return state.map((comment) => comment.id === action.payload.id ? action.payload : comment);
+		case CHANGE_COMMENT_VOTESCORE:
+			return state.map((comment) => comment.id === action.payload.id ? action.payload : comment)
+		default:
+			return state
+	}
 }
---
+++ src/reducers/PostReducer.js
@@ -0,0 +1,66 @@
+import {
+	RECEIVE_CATEGORIES,
+	RECEIVE_POSTS,
+	RECEIVE_POST,
+	SAVE_POST,
+	DELETE_POST,
+	SAVE_MODIFICATIONS,
+	CHANGE_POST_VOTESCORE,
+	SORT_BY_DIRECTION,
+	SORT_BY_CATEGORY
+} from '../actions'
+
+import _ from 'lodash'
+
+export function categories(state = [], action) {
+	switch (action.type) {
+  		case RECEIVE_CATEGORIES: 
+      		return action.categories
+    	default:
+    		return state
+  }
+}
+
+const initialPosts = {
+  posts: [],
+  postsOrderBy: 'timestamp',
+  postsOrderDirection: 'desc',
+}
+
+export function posts(state = initialPosts, action) {
+	const posts = action.posts ? action.posts : state.posts
+	const direction = action.ascDesc ? action.ascDesc : state.postsOrderDirection
+	const category = action.category ? action.category : state.postsOrderBy
+
+	switch (action.type) {
+		case RECEIVE_POSTS:
+			return { ...state, posts: _.orderBy(posts, [category],[direction]) }
+		case SAVE_POST:
+			return { ...state, posts: posts.concat(action.post) }
+		case DELETE_POST:
+		    const url = action.payload.url
+		    const id = url.substr(url.indexOf("posts/") + ("posts/".length))
+			return { ...state, posts: posts.filter((post) => id !== post.id) }
+		case SAVE_MODIFICATIONS: 
+			return { ...state, posts: posts.map((post) => post.id === action.payload.id ? action.payload : post) }
+		case SORT_BY_DIRECTION:
+			const postsOrderDirection = state.postsOrderDirection
+			return { ...state, posts: _.orderBy(posts, [category], [direction]), postsOrderDirection: direction }
+		case SORT_BY_CATEGORY:
+			const postsOrderBy = state.postsOrderBy
+			return { ...state, posts: _.orderBy(posts, [category]), postsOrderBy: category }
+		default:
+			return state
+	}
+}
+
+export function post(state = {}, action) {
+	switch (action.type) {
+		case RECEIVE_POST:
+			return action.post
+		case CHANGE_POST_VOTESCORE:
+			return action.payload
+		default:
+			return state
+	}
+}
