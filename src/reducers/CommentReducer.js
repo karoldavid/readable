e4c60b5191aee361f4f2d6f4ba9567ee26fc2f88
karoldavid <k.zysk@zoho.com>
2017-09-14 14:47:08 +0200

    fix: comment reducer access comments via state.comments

--- src/reducers/CommentReducer.js
+++ src/reducers/CommentReducer.js
@@ -23,17 +23,17 @@ export function comments(state = initialComments, action) {
 
 	switch (action.type) {
 		case ADD_COMMENT:
-			return state.concat(action.comment)
+			return { ...state, comments: comments.concat(action.comment) }
 		case GET_COMMENTS:
 			return { ...state, comments: _.orderBy(comments, [category],[direction]) }
 		case DELETE_COMMENT:
 			const url = action.payload.url
 		    const id = url.substr(url.indexOf("comments/") + ("comments/".length))
-			return comments.filter((comment) => id !== comment.id)
+			return { ...state, comments: comments.filter((comment) => id !== comment.id) }
 		case SAVE_MOD_COMMENT:
-			return comments.map((comment) => comment.id === action.payload.id ? action.payload : comment);
+			return { ...state, comments: comments.map((comment) => comment.id === action.payload.id ? action.payload : comment) }
 		case CHANGE_COMMENT_VOTESCORE:
-			return comments.map((comment) => comment.id === action.payload.id ? action.payload : comment)
+			return { ...state, comments: comments.map((comment) => comment.id === action.payload.id ? action.payload : comment) }
 		default:
 			return state
 	}
